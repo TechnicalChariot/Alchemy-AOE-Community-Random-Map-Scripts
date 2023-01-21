41e0664245a9e4ff9207e661ff77794e7b78029e
Jacob <115369420+TechnicalChariot@users.noreply.github.com>
2023-01-21 15:37:06 -0500

    Initial Release of (non-draft) Map Script

--- TechChariot_Workshop_Standalone_Maps/Bluegill/Bluegill.m
+++ TechChariot_Workshop_Standalone_Maps/Bluegill/Bluegill.m
@@ -12,44 +12,17 @@ filestruc = dir; %Extract a structure of the files in this directory
 path = filestruc.folder; path = [path(1:89)]; addpath(genpath(path)) %Adding functions in main folder to the path
 files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
 
-%%Terrain Painting --River
-%Span = [{0}];
-%Cent = [{0 0};];
-%Angle = [{0} {90}];
-%f = [{'50*sin(2*pi*x/22)'} {'50*sin(2*pi*x/29)'}];
-
-%[nSpan] = length(Span);
-%[nCent,~] = size(Cent);
-%[~,nAngl] = size(Angle);
-%[~,nfunc] = size(f);
-%
-
-%k = 0;
-%for i4 = 1:nSpan
-%for i3 = 1:nCent
-%for i2 = 1:nAngl
-%for i1 = 1:nfunc
-%[Preface,LPM_exp,SigComb] = RMS_Manual_Land(filename);
-%[River] = LandScribeV5({'B'},{0},Cent(i3,:),Angle(i2),f(i1),{1},Span(i4),[-50 50]);
-%k = k + 1;
-%RawLand(k).XY = [River];
-%COMMAND(k).XY = RMS_Processor_V4(RawLand(k).XY,LPM_exp);
-%end
-%end
-%end
-%end
-%%
 
 [Preface,LPM_exp,SigComb] = RMS_Manual_Land(filename);
 
-GILL_O = [LandScribeV5({'WO'},{0},{[0 +24]},{0},{'-0.04*x.^2'},{2},{1},[-20 20]); ...
-          LandScribeV5({'WO'},{0},{[0 -24]},{0},{'+0.04*x.^2'},{2},{1},[-20 20])];
+GILL_O = [LandScribeV5({'WO'},{0},{[0 +22]},{0},{'-0.035*x.^2'},{2},{1},[-20 20]); ...
+          LandScribeV5({'WO'},{0},{[0 -22]},{0},{'+0.035*x.^2'},{2},{1},[-20 20])];
 
 GILL_I = [LandScribeV5({'WI'},{0},{[0 +10]},{0},{'-0.020*x.^2'},{2},{1},[-16 16]);...
           LandScribeV5({'WI'},{0},{[0 -10]},{0},{'+0.020*x.^2'},{2},{1},[-16 16])];
 
-GILL_CON = [LandScribeV5({'DLC_BEACH3'},{0},{[0 +20]},{90},{'-0.04*x.^2'},{2},{1},[-6 6]); ...
-            LandScribeV5({'DLC_BEACH3'},{0},{[0 -20]},{90},{'+0.04*x.^2'},{2},{1},[-6 6])];
+GILL_CON = [LandScribeV5({'WRB'},{0},{[0 +20]},{90},{'-0.04*x.^2'},{2},{1},[-6 6]); ...
+            LandScribeV5({'WRB'},{0},{[0 -20]},{90},{'+0.04*x.^2'},{2},{1},[-6 6])];
 
 RING = [];
 
@@ -58,28 +31,44 @@ RING = [RING; LandScribeV5({'GRASS'},{1},{[0 0]},{i},{['0*x']},{1},{0},(1+40*abs
 end
 %
 
-RawLand = [GILL_CON; GILL_O; GILL_I; RING];
+RawLand = [GILL_CON; GILL_O; GILL_I; RING; Rounded_Rectangle({102},{1},{30},{'OB'},{2},{0})];
 
 COMMAND = RMS_Processor_V4(RawLand,LPM_exp);
+
+MLA = [{'L { terrain_type MUD land_position 50 50 base_size 0 land_percent 100 }'};...
+       {'L { terrain_type MUD land_position 50 35 base_size 0 land_percent 100 }'};...
+       {'L { terrain_type MUD land_position 50 65 base_size 0 land_percent 100 }'};...
+       {'L { land_position 15 15   terrain_type GRASS3 base_size 0 base_elevation 1 land_percent 100 }'}; ...
+       {'L { land_position 15 85   terrain_type GRASS3 base_size 0 base_elevation 1 land_percent 100 }'}; ...
+       {'L { land_position 85 15   terrain_type GRASS3 base_size 0 base_elevation 1 land_percent 100 }'}; ...
+       {'L { land_position 15 85   terrain_type GRASS3 base_size 0 base_elevation 1 land_percent 100 }'}; ...
+       {'L { land_position 1 1     terrain_type OB base_size 0 base_elevation 2 land_percent 100 }'}; ...
+       {'L { land_position 1 99    terrain_type OB base_size 0 base_elevation 2 land_percent 100 }'}; ...
+       {'L { land_position 99 1    terrain_type OB base_size 0 base_elevation 2 land_percent 100 }'}; ...
+       {'L { land_position 99 99   terrain_type OB base_size 0 base_elevation 2 land_percent 100 }'}];
+
+
+
+
 List = COMMAND;
 
 G = []; C = [];
 
-r = [32]; off = [45]; adc = [180]; SA  = [90]; b  = [0]; ecc = [1];
+r = [33]; off = [40 42 44 46 48 50];
+adc = [170 172 174 176 178 180 182 184 186 188 190];
+SA  = [0 90]; b  = [0 0.05 0.1]; ecc = [1];
 G = [{r}; {off}; {adc}; {SA}; {b}; {ecc}];
 
 
-BE = 1; BS = 7; LP = 0; ZA = 0; delta = 0;
+BE = 1; BS = 6; LP = 0; ZA = 0; delta = 0;
 C = [{BE}; {BS}; {LP}; {ZA}; {delta}];
 
 [create_player_lands] = RMS_CPL_V7(G,C);
 
-%[List] = RMS_RS_V3(f,Angle,{'C'},COMMAND);
-CODE = [Preface; List; create_player_lands]; %Adding Preface, Definitions, Random Statement to beginning of CODE
+CODE = [Preface; List; MLA; create_player_lands]; %Adding Preface, Definitions, Random Statement to beginning of CODE
 RMS_ForgeV4(filename,CODE);
 
-
-%ObjectAutoscribeV8('ObjectDatabase.ods')
+%ObjectAutoscribeV8('Bluegill.ods')
 
 disp(["Run Completed " datestr(clock) "..."])
 toc
