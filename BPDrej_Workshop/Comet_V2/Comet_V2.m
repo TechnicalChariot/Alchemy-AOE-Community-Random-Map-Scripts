15259a0aac5275121fc005ed0385dd85b88710f5
Jacob <115369420+TechnicalChariot@users.noreply.github.com>
2023-02-11 15:00:34 -0500

    Simplified the player land creation process with upgraded version of player land direct placement tool (CPL_V9)

--- BPDrej_Workshop/Comet_V2/Comet_V2.m
+++ BPDrej_Workshop/Comet_V2/Comet_V2.m
@@ -15,25 +15,6 @@ files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
 
 [Preface,LPM_exp,~] = RMS_Manual_Land(filename);
 
-%off =  [45];           %Player Starting Offset Angle
-%SA  =  [0];           %Player Starting Seed Angle
-%b   =  [0.0];          %Player Starting team bias factor
-%r   =  [10];           %Player Starting Group radius
-%e   = 0.5; e = 2*e;    %Player Starting Eccentricity
-%
-%[create_player_lands] = RMS_CPL_V2({1},{'GRASS'},{0},off,SA,b,r,e);
-
-
-
-##TL1 = LandScribeV5([{'GRASS'}],[{1}],{[0 0]},{45},{'0*x'},{1},{1},sqrt(2)*[-50 50]);
-##TL2 = LandScribeV5([{'DIRT'}],[{1}],{[0 0]},{-45},{'0*x'},{1},{1},sqrt(2)*[-50 50]);
-##TL = [TL1; TL2];
-
-
-
-##PLI = [PLI1;PLI2;PLI3;PLI4];
-
-##TL = []
 b = 30;
 ecc = 0.30;
 for i=1:360
@@ -43,61 +24,35 @@ y(i,1) = r(i,1)*sind(i);
 end
 %
 M = [x y]; CC = [62 62];
-tail = LandScribeV5([{'GRASS'}],[{0}],{[0 -30]},{-45},{'0*x'},{1},{60},[-20 20;-50 50]);
+%tail = LandScribeV5([{'GRASS'}],[{0}],{[0 -30]},{-45},{'0*x'},{1},{60},[-20 20;-50 50]);
 TL = LandScribeV5([{'DIRT'}],[{1}],{CC},{45},{M},{1});
 tag = [{'if P2'};{'elseif P4'};{'elseif P6'};{'elseif P8'};{'endif'}];
-COMMAND = [];
-PLI1 = LandScribeV5([{'DIRT'}],[{1}],{[00 17]},{-45},{'0*x'},{1},{0},[-18 18]);
-PLI2 = LandScribeV5([{'DIRT'}],[{1}],{[00 -50]},{-45},{'0*x'},{1},{0},[-2 2]);
-PLI3 = LandScribeV5([{'DIRT'}],[{1}],{[00 -50]},{-45},{'0*x'},{1},{0},[-2 2]);
-PLI4 = LandScribeV5([{'DIRT'}],[{1}],{[00 -50]},{-45},{'0*x'},{1},{0},[-2 2]);
-for i = 1:5
-if i == 1
-COMMAND = [COMMAND; tag(i)];
-COMMAND = [COMMAND; RMS_Processor_V4([TL; tail],LPM_exp,[{PLI1} {PLI2} {PLI3} {PLI4}])];
-elseif i == 2
-COMMAND = [COMMAND; tag(i)];
-PLI1 = LandScribeV5([{'DIRT'}],[{1}],{[00 1]},{-45},{'0*x'},{1},{0},[-16 16]);
-PLI2 = LandScribeV5([{'DIRT'}],[{1}],{[00 30]},{-45},{'0*x'},{1},{0},[-16 16]);
-COMMAND = [COMMAND; RMS_Processor_V4([TL; tail],LPM_exp,[{PLI1} {PLI2} {PLI3} {PLI4}])];
-elseif i == 3
-COMMAND = [COMMAND; tag(i)];
-PLI1 = LandScribeV5([{'DIRT'}],[{1}],{[00 -1]},{-45},{'0*x'},{1},{0},[-16 16]);
-PLI2 = LandScribeV5([{'DIRT'}],[{1}],{[00 16]},{-45},{'0*x'},{1},{0},[-20 20]);
-PLI3 = LandScribeV5([{'DIRT'}],[{1}],{[00 33]},{-45},{'0*x'},{1},{0},[-16 16]);
-COMMAND = [COMMAND; RMS_Processor_V4([TL; tail],LPM_exp,[{PLI1} {PLI2} {PLI3} {PLI4}])];
-elseif i == 4
-COMMAND = [COMMAND; tag(i)];
-PLI1 = LandScribeV5([{'DIRT'}],[{1}],{[00 -1]},{-45},{'0*x'},{1},{0},[-16 16]);
-PLI2 = LandScribeV5([{'DIRT'}],[{1}],{[00 10]},{-45},{'0*x'},{1},{0},[-20 20]);
-PLI3 = LandScribeV5([{'DIRT'}],[{1}],{[00 22]},{-45},{'0*x'},{1},{0},[-20 20]);
-PLI4 = LandScribeV5([{'DIRT'}],[{1}],{[00 33]},{-45},{'0*x'},{1},{0},[-16 16]);
-COMMAND = [COMMAND; RMS_Processor_V4([TL; tail],LPM_exp,[{PLI1} {PLI2} {PLI3} {PLI4}])];
-else
-COMMAND = [COMMAND; tag(i)];
-end
-%
+
+%% -- CPL_V9 INPUT FORMAT -- %%
+% G = [{Vector of Radii}; ...
+%      {Vector of Angular Offsets Between Flank and Pocket}; ...
+%      {Vector of Angular Distance to Centroid of Teams}; ...
+%      {Vector of Clocking "Seed Angles"}; ...
+%      {Vector of Team Biases}; ...
+%      {Vector of Eccentricities}; ...
+%      {Matrix of Team Centers}] (geometric inputs)
+
+% C = [{Base Elevation}; ...
+%      {Base Size}; ...
+%      {Number of Tiles}; ...
+%      {Zone Avoidance}; ...
+%      {Linear Slop};
+%      {[left right top bottom] border avoidances}]  (characteristic inputs)
+
+G = [{30}; {45}; {180}; {45}; {[0.3]}; {0.6}; {[CC; CC]}];
+C = [{1}; {0}; {14400}; {0}; {0}; {[0 0 0 0]}];
+
+[create_player_lands] = RMS_CPL_V9(G,C);
 
 
+COMMAND = [RMS_Processor_V4([TL],LPM_exp); create_player_lands];
 
-end
-%
-MLA = {['L { base_size 0 terrain_type DIRT base_elevation 1 land_position ' num2str(CC(1)) ' ' num2str(CC(2)) ' }']};
-
-
-##O = [45];
-##%R = 38;
-##R = 30;
-##S = 15;
-
-
-##if i5 == 1
-##[Size_List] = [size_prefix; RMS_RS_V3(O,{'C'},COMMAND)];
-##else
-##[Size_List] = [Size_List; size_prefix; RMS_RS_V3(O,{'C'},COMMAND)];
-##end
-##%
-
+MLA = [];
 
 
 %ObjectAutoscribeV8('ObjectDatabase.ods')
@@ -107,43 +62,3 @@ RMS_ForgeV4(filename,CODE);
 disp(["Run Completed " datestr(clock) "..."])
 toc
 
-##k = 1;
-##for j = 1:length(O)
-##
-##c1 = [25 25];
-##c2 = [25 75];
-##c3 = [75 25];
-##c4 = [75 75];
-##
-##Y1 = (c1(:,1)-50)*sind(O(j))+(c1(:,2)-50)*cosd(O(j));
-##Y2 = (c2(:,1)-50)*sind(O(j))+(c2(:,2)-50)*cosd(O(j));
-##Y3 = (c3(:,1)-50)*sind(O(j))+(c3(:,2)-50)*cosd(O(j));
-##Y4 = (c4(:,1)-50)*sind(O(j))+(c4(:,2)-50)*cosd(O(j));
-##
-##X1 = (c1(:,1)-50)*cosd(O(j))-(c1(:,2)-50)*sind(O(j));
-##X2 = (c2(:,1)-50)*cosd(O(j))-(c2(:,2)-50)*sind(O(j));
-##X3 = (c3(:,1)-50)*cosd(O(j))-(c3(:,2)-50)*sind(O(j));
-##X4 = (c4(:,1)-50)*cosd(O(j))-(c4(:,2)-50)*sind(O(j));
-##
-##PLI1 = LandScribeV5([{'NNRB'}],[{1}],{[00 -Y1]},{O(j)},{'0*x'},{1},{0},X1+[-S +S]);
-##PLI2 = LandScribeV5([{'NNRB'}],[{1}],{[00 -Y2]},{O(j)},{'0*x'},{1},{0},X2+[-S +S]);
-##PLI3 = LandScribeV5([{'NNRB'}],[{1}],{[00 -Y3]},{O(j)},{'0*x'},{1},{0},X3+[-S +S]);
-##PLI4 = LandScribeV5([{'NNRB'}],[{1}],{[00 -Y4]},{O(j)},{'0*x'},{1},{0},X4+[-S +S]);
-##
-##Structure(k).XY = [LandScribeV5([{'NNRB'}],[{0}],{25 25},{O(j)},{M1},{1}); ...
-##                   LandScribeV5([{'WNI'}],[{0}],{25 25},{O(j)},{M2},{1}); ...
-##                   LandScribeV5([{'NNRB'}],[{0}],{25 75},{O(j)},{M1},{1}); ...
-##                   LandScribeV5([{'WNI'}],[{0}],{25 75},{O(j)},{M2},{1}); ...
-##                   LandScribeV5([{'NNRB'}],[{0}],{75 25},{O(j)},{M1},{1}); ...
-##                   LandScribeV5([{'WNI'}],[{0}],{75 25},{O(j)},{M2},{1}); ...
-##                   LandScribeV5([{'NNRB'}],[{0}],{75 75},{O(j)},{M1},{1}); ...
-##                   LandScribeV5([{'WNI'}],[{0}],{75 75},{O(j)},{M2},{1})];
-##
-##%Boundary (k).XY = [LandScribeV5([{'WNI'}],[{0}],{25 25},{O(j)},{M3},{1}); ...
-##%                   LandScribeV5([{'WNI'}],[{0}],{25 75},{O(j)},{M3},{1}); ...
-##%                   LandScribeV5([{'WNI'}],[{0}],{75 25},{O(j)},{M3},{1}); ...
-##%                   LandScribeV5([{'WNI'}],[{0}],{75 75},{O(j)},{M3},{1})];
-##%
-##%
-##%COMMAND(k).XY = [RMS_Processor_V4(Structure(k).XY,LPM_exp,[{PLI1} {PLI2} {PLI3} {PLI4}]); RMS_Processor_Random_Coord(Boundary(k).XY,LPM_exp,1)];
-##k = k + 1;
