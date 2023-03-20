4c76a53faa407dad961f0f9120023da237cd32ee
bpdrej <116236860+bpdrej@users.noreply.github.com>
2023-03-20 19:04:06 -0400

    Work on Eagle-Arabia

--- Return_of_the_Rising_Eagle_Cup/RREC_Arabia/RREC_Arabia.m
+++ Return_of_the_Rising_Eagle_Cup/RREC_Arabia/RREC_Arabia.m
@@ -6,17 +6,26 @@ clear all
 close all
 clc
 
+pkg load image
 
 tic
 disp(["Run Executed " datestr(clock) "..."])
 
 filestruc = dir; %Extract a structure of the files in this directory
-path = filestruc.folder; path = path(1:102); addpath(genpath(path)) %Adding functions in main folder to the path
+path = filestruc.folder; path = path(1:90); addpath(genpath(path)) %Adding functions in main folder to the path
 files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
 
 [Preface,LPM_exp,~] = RMS_Manual_Land(filename);
-
-LPM_exp = LPM_Shadow(LPM_exp,5);
+##L = 'C:\Program Files (x86)\Steam\steamapps\common\AoE2DE\resources\_common\random-map-scripts\Age-of-Empires-II-Definitive-Edition----Random-Map-Scripts\Return_of_the_Rising_Eagle_Cup\RREC_Arabia\eagle.png'
+L = 'C:\Program Files (x86)\Steam\steamapps\common\AoE2DE\resources\_common\random-map-scripts\Age-of-Empires-II-Definitive-Edition----Random-Map-Scripts\Return_of_the_Rising_Eagle_Cup\RREC_Arabia\eaglerot.png'
+f = 1;
+M = ImagetoLands(f,L);
+C = [50 50];
+MLA = [{['L { terrain_type DIRT base_size 0 land_position ' num2str(C(1)) ' ' num2str(C(2)) ' } ']}];
+##B = LandScribeV5([{'DIRT'}],[{0}],{[45 55]},{135},{M},{0});
+B = LandScribeV5([{'DIRT'}],[{0}],{C},{0},{M},{0});
+COMMAND = RMS_Processor_V4(B,LPM_exp);
+##LPM_exp = LPM_Shadow(LPM_exp,5);
 
 % G = [{Vector of Radii}; ...
 %      {Vector of Angular Offsets Between Flank and Pocket}; ...
@@ -49,7 +58,7 @@ LPM_exp = LPM_Shadow(LPM_exp,5);
 %ObjectAutoscribeV8('ObjectDatabase.ods')
 %CODE = [Preface; Size_List; MLA]; %Adding Preface, Definitions, Random Statement to beginning of CODE
 
-CODE = [];
+CODE = [Preface; COMMAND; MLA];
 RMS_ForgeV4(filename,CODE);
 
 disp(["Run Completed " datestr(clock) "..."])
