805bb4f1af2f2d916e385c096297ba6a6dfa8151
Jacob <115369420+TechnicalChariot@users.noreply.github.com>
2023-04-03 20:14:24 -0400

    Changed Centurion to Scout on Akropole, Kresphugeto. Improved relics and added inner pocket to central forest on Krephugeto

--- Return_of_the_Rising_Eagle_Cup/RREC_Kresphugeto/RREC_Kresphugeto.m
+++ Return_of_the_Rising_Eagle_Cup/RREC_Kresphugeto/RREC_Kresphugeto.m
@@ -10,8 +10,9 @@ clc
 tic
 disp(["Run Executed " datestr(clock) "..."])
 
-str = pwd; str = str(1:102); addpath(str)
-[filename] = Load_Library();
+filestruc = dir; %Extract a structure of the files in this directory
+path = filestruc.folder; path = path(1:90); addpath(genpath(path)) %Adding functions in main folder to the path
+files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
 [Preface,LPM_exp,~] = RMS_Manual_Land(filename);
 
 MLA = [{'L { terrain_type BT land_percent 100 land_position  1  1 }'}; ...
@@ -88,4 +89,4 @@ disp(["Run Completed " datestr(clock) "..."])
 toc
 
 
-%ObjectAutoscribeV8('Krupsona.ods')
+%ObjectAutoscribeV8('Kresphugeto.ods')
