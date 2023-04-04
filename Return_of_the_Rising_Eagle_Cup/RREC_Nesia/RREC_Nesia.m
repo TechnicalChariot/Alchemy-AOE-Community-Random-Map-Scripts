70911b049c203815dfea8e11280e447dd98e2fb2
Jacob <115369420+TechnicalChariot@users.noreply.github.com>
2023-04-04 08:56:44 -0400

    Added Relics to Nesia, refined resource spawns  on player-starting islands, removed most food from outlying berries

--- Return_of_the_Rising_Eagle_Cup/RREC_Nesia/RREC_Nesia.m
+++ Return_of_the_Rising_Eagle_Cup/RREC_Nesia/RREC_Nesia.m
@@ -11,10 +11,8 @@ tic
 disp(["Run Executed " datestr(clock) "..."])
 
 filestruc = dir; %Extract a structure of the files in this directory
-path = filestruc.folder; path = path(1:102); addpath(genpath(path)) %Adding functions in main folder to the path
+path = filestruc.folder; path = path(1:89); addpath(genpath(path)) %Adding functions in main folder to the path
 files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
-
-
 [Preface,LPM_exp,~] = RMS_Manual_Land(filename);
 
 MLP = []; MLA = [];
