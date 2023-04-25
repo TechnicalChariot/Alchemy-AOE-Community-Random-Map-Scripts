7ede91b85768a37b4212a46ea5b564e8db83ae97
Jacob <115369420+TechnicalChariot@users.noreply.github.com>
2023-04-24 22:45:57 -0400

    Arcticpelago some function work

---
+++ TechChariot_Workshop_Standalone_Maps/Arcticpelago/Arcticpelago.m
@@ -0,0 +1,58 @@
+%Arcticpelago Land Generation
+%TechChariot
+%4.23.23
+
+clear all
+close all
+clc
+
+
+tic
+disp(["Run Executed " datestr(clock) "..."])
+
+filestruc = dir; %Extract a structure of the files in this directory
+path = filestruc.folder; path = path(1:89); addpath(genpath(path)) %Adding functions in main folder to the path
+files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
+
+[Preface,LPM_exp,~] = RMS_Manual_Land(filename);
+
+MLP = []; f = [{'0.005*x.^2'}];
+%f = [{'0*x'}; {'0.010*x.^2'}; {'-0.010*x.^2'}];
+
+
+[IsX,IsY] = function_to_points([f(1); {[50 50]}; {50}],[{[-50 50]}; {5}]);
+
+
+
+
+%for i = 1:length(O)
+%if i == 1
+%[IsX,IsY] = function_to_points([f(1); {c}; {O(i)}],[{V}; {n}]);
+%else
+%[IsXn,IsYn] = function_to_points([f(1); {c}; {O(i)}],[{V}; {n}]);
+%IsX = [IsX; IsXn]; IsY = [IsY; IsYn];
+%end
+%end
+%%
+
+Is.TT = [{'WRB'}]; Is.BS = [1];
+Is.X = [IsX]; Is.Y = [IsY];
+Is.NT = 16;
+
+[LM_Is,Is] = LandScribeV6(Is,[1 1]); List = [RMS_Processor_V6(LM_Is)];
+
+
+
+MLA = [];
+
+
+
+CODE = [Preface; MLP; List; MLA]; %Adding Preface, Definitions, Random Statement to beginning of CODE
+
+RMS_ForgeV4(filename,CODE);
+
+disp(["Run Completed " datestr(clock) "..."])
+toc
+
+
+%ObjectAutoscribeV8('GitcheeGumee.ods')
