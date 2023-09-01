88798161e034453649facb951534c22f25491c42
Jacob <115369420+TechnicalChariot@users.noreply.github.com>
2023-08-31 22:41:49 -0400

    continued work on arcticpelago

--- TechChariot_Workshop_Standalone_Maps/Arcticpelago/Arcticpelago.m
+++ TechChariot_Workshop_Standalone_Maps/Arcticpelago/Arcticpelago.m
@@ -19,19 +19,65 @@ files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
 MLP = [];
 
 %% -- Section on Islands -- %%
-R   = [100];    %Radius of the Curve
-THK = [040];
+R   = [090];    %Radius of the Curve
+THK = [090];
 ISf = [{['sqrt(' num2str(R) '^2-x.^2)']}];
 
 %ISf = [{'0.01*x.^2'}];
 
-[ISX,ISY] = function_to_points_V3([ISf; {[100 000]}; {45}],[-R R],[-THK THK],[10 10; 10 10],[10 00],[{"edge"},{"edge"}]);
+[ISX,ISY,dx,dy] = function_to_points_V3([ISf; {[100 000]}; {45}],[-R R],[-THK/2 THK/2],[10 10; 10 10],[10 00],[{"edge"}],[3]);
+
+[nx,ny] = size(ISX); %Garnering the size of the point matrix
+
+maxperdiff = 100; tol = 1;
+
+Ht = 100; %the value of the field at the top
+Hb = 000; %the value of the field at the bottom
+Hl = 060; %the value of the field at the left
+Hr = 000; %the value of the field at the right
+
+H = (Ht+Hb+Hl+Hr)/4*ones(nx,ny); %initializing the general field
+
+H(1,:)   = Ht;
+H(end,:) = Hb;
+H(:,1)   = Hr;
+H(:,end) = Hl;
+
+H(1,1)     = (Hr + Ht)/2;
+H(1,end)   = (Hl + Ht)/2;
+H(end,1)   = (Hr + Hb)/2;
+H(end,end) = (Hl + Hb)/2;
+
+
+
+while maxperdiff > tol
+Hold = H; %Initialize the old value of H
+  for j = 2:ny-1
+    for i = 2:nx-1
+      H(i,j) = 0.5*(dx(i,j)*dy(i,j))^2/(dx(i,j)^2+dy(i,j)^2)*((H(i-1,j)+H(i+1,j))/dx(i,j)^2+(H(i,j-1)+H(i,j+1))/dy(i,j)^2);
+    end
+    %
+  end
+  %
+  maxperdiff = max(max(100*abs((H-Hold)./H)))
+end
+%
+
+figure(1)
+mesh(H')
+
+
+
+
+IS.NT = 10;
+
+
 IS.X = [ISX]; IS.Y = [ISY];
+
 IS.BE = [0];
 IS.TT = [{'DLC_WETROCKBEACH'}];
 %IS.SS = [3];
-IS.BS = [1];
-IS.NT = [0];
+IS.BS = [2];
 %IS.Z = 1;
 
 
