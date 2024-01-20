76bc49b19b1c24b9be477a4df8fb6133cef58cb6
Jacob <115369420+TechnicalChariot@users.noreply.github.com>
2024-01-19 23:32:53 -0500

    Updated to include large, orderly hydroponics basin in central area

--- QFilip/SpaceColony/SpaceColony.m
+++ QFilip/SpaceColony/SpaceColony.m
@@ -17,7 +17,7 @@ files = {filestruc.name}; [filename] = RMS_GetLatest(files,'rms');
 [Preface,LPM_exp,~] = RMS_Manual_Land(filename);
 
 % -- Central Area Permeter -- %
-CAP.TT = {'INFRAS'}; %Terrain Type for Central Area Perimeter
+CAP.TT = {'FRM'}; %Terrain Type for Central Area Perimeter
 CAP.BS = {2}; %Base Size
 CAP.NT = {0}; %Number of Tiles
 CAP_R = 15; %Central Area Perimeter Radius
@@ -29,7 +29,7 @@ CAP.X = M(:,1); CAP.Y = M(:,2); %Extracting X and Y Coordinates
 [LM_CAP,CAP] = LandScribeV6(CAP,[1 1]);
 
 % -- Central Area Fill -- %
-CAF.TT = {'INFRAS'}; %Terrain Type for Central Area Perimeter
+CAF.TT = CAP.TT; %Terrain Type for Central Area Perimeter
 CAF.BS = {0}; %Base Size
 CAF.X = {50}; CAF.Y = {50}; %Coordinates
 [LM_CAF,CAF] = LandScribeV6(CAF,[1 1]);
@@ -65,4 +65,4 @@ RMS_ForgeV4(filename,CODE);
 disp(["Run Completed " datestr(clock) "..."])
 toc
 
-%ObjectAutoscribeV9('Petra.ods')
+%ObjectAutoscribeV9('SpaceColony_V2.ods')
